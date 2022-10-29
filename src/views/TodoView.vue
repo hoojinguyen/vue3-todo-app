fb65493c4b2748152b7987551137e5359fb10b59
hoojinguyen <vanhoinguyen98@gmail.com>
2022-10-29 11:50:01 +0700

    create todo page

---
+++ src/views/TodoView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="todo">
+    <h1>This is a todo page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .todo {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>

b68305442d3169615ae661dc39173637f4457bd8
hoojinguyen <vanhoinguyen98@gmail.com>
2022-11-27 10:47:31 +0700

    chore: test husky and lint-staged

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,11 @@
 
 <script setup lang="ts">
 import MainLayout from "@/layouts/MainLayout.vue"
-import { initThinBackend } from "thin-backend"
+import { initThinBackend }                              from "thin-backend"
 import { ThinBackend } from "thin-backend-vue"
-import { RouterView } from "vue-router"
+                                    import { RouterView } from "vue-router"
+
+const A = ""
 
 initThinBackend({
     host: import.meta.env.VITE_THINBACKEND_URL,
